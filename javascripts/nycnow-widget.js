e0c1750bd53528e7ec504ddce2eac90e329da09f
Nathan Kozyra <nkozyra@gmail.com>
2017-10-21 12:43:12 -0400

    adds straus URLs

---
+++ javascripts/nycnow-widget.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+<script src="https://dist.pointslocal.com/pointslocal.widgets.min.js"></script><link rel="stylesheet" type="text/css" href="https://dist.pointslocal.com/pointslocal.widgets.min.css">
+<div id="pointslocal-rail-search"></div>
+<!-- Rail search widget -->
+<script> var searchWidget = new Pointslocal('#pointslocal-rail-search', { widget: 'search', companion: { 'key':'events','widget':'events', 'options': { 'count': 3, 'featured': 1 } }, vars: { 'title': 'Local Events', 'container_class': 'pointslocal-widget' }, stylesheet: 'straus', template: 'pointslocal.search.mini' }); </script>
+
+</body>
+</html>
